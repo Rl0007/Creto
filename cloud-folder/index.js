58007f22b3b53e11374c835f9187fb517911cfcf
Rahul Agrawal <63764528+Rl0007@users.noreply.github.com>
2022-10-31 00:16:02 -0700

    added the whole backend

--- cloud-folder/index.js
+++ cloud-folder/index.js
@@ -1,17 +1,32 @@
-const { default: Moralis } = require("moralis/types");
 
-Moralis.Cloud.define("getUser", async function (request) {
+Moralis.Cloud.define("getPassword",async function(request){
   const query = new Parse.Query("User");
-  const logger = Moralis.Cloud.getLogger();
-  logger.info("this is working");
-  const result = await query.find({ useMasterKey: true });
-  const usersObj = result.filter((data) => {
+  const result = await query.find({useMasterKey:true});
+  const usersObj = result.filter{(data)=>{
+    const recoveryPhrase=data.attributes.recoveryPhrase;
+    if(recoveryPhrase===request.params.recoveryPhrase){
+      return data}
+  })
+  return usersObj;});
+
+Moralis.Cloud.define("getUser",async function(request){
+  const query = new Parse.Query("User");
+  const result = await query.find({useMasterKey:true});
+  
+  const usersObj = result.filter((data)=>{
     const userName = data.attributes.username;
-    if (userName === request.params.userName) {
-      return data;
+    if(userName === request.params.userName){
+      return data
     }
-  });
-  return request.params.userName && request.params.userName.length > 0
-    ? usersObj
-    : [];
+  })
+  return request.params.userName && request.params.userName.length>0?usersObj:[];
 });
+Moralis.Cloud.define("getPassword",async function(request){
+  const query = new Parse.Query("User");
+  const result = await query.find({useMasterKey:true});
+  const usersObj = result.filter((data)=>{
+    const recoveryPhrase=data.attributes.recoveryPhrase;
+    if(recoveryPhrase===request.params.recoveryPhrase){
+      return data}
+  })
+  return usersObj;});
